8694a6f77308923cee48db2a9dcf5cef169cee9b
Nitin Rana <nitinrana2519@gmail.com>
2026-02-16 19:31:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function volume_sphere() {
+function volume_sphere(e) {
+	e.preventDefault()
     //Write your code here
-  
+	let value = radius.value
+	let volume = ((4/3) * Math.PI * value**3).toFixed(4)
+	if(value != ''){
+		volumeField.value = !parseInt(value) || value[0] == '-' ? NaN : volume;
+	}
 } 
-
+let radius = document.getElementById('radius')
+let volumeField = document.getElementById('volume')
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
